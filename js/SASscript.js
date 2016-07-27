97022ae37d245e4f5bfd7d9c0402f9ce09a49f3d
Gabriel Lucas <gabriellucas71@gmail.com>
2016-07-27 17:13:51 +0200

    Second Commit

--- js/SASscript.js
+++ js/SASscript.js
@@ -1,252 +1,124 @@
 //--- Load Audio files and position variables.
-var dragStatus;
-var degrees = 0;
-var demo = document.getElementById('demo');
+var Xdegree = 0;
+var Ydegree = 90;
 var context = window.AudioContext = window.AudioContext || window.webkitAudioContext;
+context = new AudioContext();
 var audioList = ['', 'audio/effect1.wav', 'audio/effect2.wav', 'audio/effect3.wav', 'audio/effect4.wav' ];
 var audiostatus = 'off';
-var listener = {x : '',	y : '', id : ''};
-listener.id = document.getElementById('head');
-context = new AudioContext();
+var listener = {x : '',	y : ''};
 var contextListener = context.listener;
-var audio = new Array(4);
+var audio = new Array();
 var source = new Array(4);
 var sound = new Array(4);
 var panner = new Array(4);
 for (var i=1; i<5; i++){
-	audio[i] = new Audio();
-	audio[i].src = audioList[i];
-	audio[i].loop = true;
+	var baseString = "#Src";
+	audio[baseString + i] = new Audio();
+	audio[baseString + i].src = audioList[i];
+	audio[baseString + i].loop = true;
 	panner[i] = context.createPanner();
-	panner[i].connect(context.destination);
-	source[i] = {x : '', y : '', src : '', distX : '', distY : '' };
+	panner[i].connect(context.destination);	
+	source[i] = {x : '', y : '', src : '' };
 	source[i].src = document.getElementById("Src" + i);
-	sound[i] = context.createMediaElementSource(audio[i]);
+	sound[i] = context.createMediaElementSource(audio[baseString + i]);
 	sound[i].connect(panner[i]);
+	panner[i].setPosition(source[i].src.offsetLeft, source[i].src.offsetTop, 1);
 }
 
 //Listener attributes.
-
+console.log(Xdegree,Ydegree,Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)))
 contextListener.setPosition(460,300,0);
-contextListener.setOrientation(degrees,degrees+90,-1,0,1,0);
+contextListener.setOrientation(Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)),-1,0,1,0);
 
 //--- Drag elements with defined classes.
-function startDrag(e) {
+$("#head").draggable({
+	containment: "#grid",
+	scroll: false,	
+	start:function(event, ui) {
+		$(this).addClass('noclick');
+	},
+	drag: positionListener
+});
 
-	dragStatus = 1;
+$(".src").draggable({
+	containment: "#grid",
+	scroll: false,	
+	start: function(event, ui) {
+		$(this).addClass('noclick');
+	},
+	drag: positionSources
+});
 
-	//Determine event object.
-	if (!e) {
-		var e = window.event;
-	}
-	if(e.preventDefault) e.preventDefault();
-
-	// IE uses srcElement, others use target.
-	targ = e.target ? e.target : e.srcElement;
-	if (targ.className != 'draggable' ) {
-		if (targ.className != 'src draggable' ) {return};
-	};
-
-	//Calculate event X, Y coordinates.
-	offsetX = e.clientX;
-	offsetY = e.clientY;
-
-	//Assign default values for top and left properties.
-	if(!targ.style.left) { targ.style.left='0px'};
-	if (!targ.style.top) { targ.style.top='0px'};
-
-	//Calculate integer values for top and left properties.
-	coordX = parseInt(targ.style.left);
-	coordY = parseInt(targ.style.top);
-	drag = true;
-
-	//Move element.
-	document.onmousemove = dragDiv;
-	return false;
+function positionListener(event, ui){
+	listener.x = ui.position.left;
+	listener.y = ui.position.top;
+	console.log(listener.x, listener.y);
+	contextListener.setPosition(listener.x, listener.y, 0);
 }
 
-function dragDiv(e) {
-	if (!drag) {return};
-	if (!e) { var e= window.event};
+function positionSources(event, ui){
+	console.log(event.target.id, ui.position.left, ui.position.top)
+	var src = event.target.id;
+	var srcNumber = src.match(/\d/g);
 
-
-	positionSources()
-	positionListener()
-
-	//Move element
-	targ.style.left=coordX+e.clientX-offsetX+'px';
-	targ.style.top=coordY+e.clientY-offsetY+'px';
-
-	return false;
-}
-
-function stopDrag() {
-	drag=false;
-	dragStatus = 0;
-}
-window.onload = function() {
-	document.onmousedown = startDrag;
-	document.onmouseup = stopDrag;
-}
-
-function orientationListener(){ //The orientation will change as the Listener turns.
-	//contextListener.setOrientation(0,0,-1,0,1,0);
-}
-
-function positionListener(){
-	obj = listener.id;
-	listener.x = obj.offsetLeft;
-	listener.y = obj.offsetTop;
-	contextListener.setPosition(listener.x,listener.y,0);
-	console.log(listener.x, listener.y)
-}
-
-function positionSources(){
-	for (var i=1; i<5; i++){
-		obj = source[i].src;
-		source[i].x = obj.offsetLeft;
-		source[i].y = obj.offsetTop;
-		panner[i].setPosition(source[i].x, source[i].y, 1);
-	}
-}
-
-//$( "head" ).draggable({ containment: "#grid-snap", scroll: false }) // Easier with Jquery.
-
-source[1].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[1].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[1].src.classList.add('srcplay');
-			//Play it.
-			audio[1].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[1].src.classList.add('srcplay');
-			audio[1].play();
-		}
-	} else if (source[1].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[1].pause();	
-		source[1].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
-}
-
-source[2].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[2].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[2].src.classList.add('srcplay');
-			//Play it.
-			audio[2].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[2].src.classList.add('srcplay');
-			audio[2].play();
-		}
-	} else if (source[2].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[2].pause();	
-		source[2].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
-}
-
-source[3].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[3].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[3].src.classList.add('srcplay');
-			//Play it.
-			audio[3].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[3].src.classList.add('srcplay');
-			audio[3].play();
-		}
-	} else if (source[3].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[3].pause();	
-		source[3].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
-}
-
-source[4].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[4].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[4].src.classList.add('srcplay');
-			//Play it.
-			audio[4].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[4].src.classList.add('srcplay');
-			audio[4].play();
-		}
-	} else if (source[4].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[4].pause();	
-		source[4].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
+	srcNumber = parseInt(srcNumber.join(""));
+	source[srcNumber].x = ui.position.left;
+	source[srcNumber].y = ui.position.top;
+	panner[srcNumber].setPosition(source[srcNumber].x, source[srcNumber].y, 1);
 }
 
 //--- Turn listener.
-listener.id.onclick = function(){
-	if (dragStatus == 0){
-		degrees += 90;
-		this.style.transform = "rotate("+degrees+"deg)";
-		console.log(degrees);
-		contextListener.setOrientation(sen,-1,0,1,0);
+$('#head').click(function() {
+	if ($(this).hasClass('noclick')){
+		$(this).removeClass('noclick');
+	}else {
+		Xdegree += 90;
+		Ydegree += 90;		
+		console.log(Xdegree,Ydegree,Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)))
+		this.style.transform = "rotate("+-Xdegree+"deg)";
+		contextListener.setOrientation(Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)),-1,0,1,0);
 	}
-}
+});
 
 //--- Demonstration function.
-demo.onclick = function(){
-	console.log (' Demo Function')
-	while (listener.id.offsetLeft < 122 && listener.id.offsetop > 314){ //First Move.
-		listener.id.offsetLeft -= 1;
-		listener.id.offsetTop += 1;
+$('#demo').click(function() {
+	console.log ('Demo Function')
+});
+
+//--- Play/Stop Sound sources on click.
+$('.src').click(function(event, ui) {
+	if ($(this).hasClass('noclick')) {
+		$(this).removeClass('noclick');
+	}else {
+		var Source = String('#'+ event.target.id);
+		var song;
+
+		song = audio[Source];
+		console.log();
+		playPause(event, song, audiostatus)
 	}
+});
 
-	while (listener.id.offsetLeft < XXX && listener.id.offsetop > YYY){ //Second Move.
-		listener.id.offsetLeft -= 1;
-		listener.id.offsetTop += 1;
+function playPause(event, song, audiostatus){
+	var Source = String('#'+ event.target.id);
+	console.log(Source)
+	//who is Source
+	if (!$(Source).hasClass("srcplay")) {
+		console.log(Source)
+		if (audiostatus == 'off') {
+			console.log(Source)
+			$(Source).addClass('srcplay');
+			//Play it.
+			song.play();
+			return false;
+		} else if (audiostatus == 'on') {
+			$(Source).addclass('srcplay');
+			song.play();
+		}
+	} else {
+		//Stop it.
+		song.pause();	
+		$("Source").removeClass('srcplay');
+		audiostatus = 'off';
 	}
-
-	degrees += 90;
-	listener.id.style.transform = "rotate("+degrees+"deg)";
-
-	while (listener.id.offsetLeft < XXX && listener.id.offsetop > YYY){ //Third Move.
-		listener.id.offsetLeft -= 1;
-		listener.id.offsetTop += 1;
-	}
-
-	degrees += 90;
-	listener.id.style.transform = "rotate("+degrees+"deg)";
-
-	while (listener.id.offsetLeft < XXX && listener.id.offsetop > YYY){ //Forth Move.
-		listener.id.offsetLeft -= 1;
-		listener.id.offsetTop += 1;
-	}
-
-	degrees += 90;
-	listener.id.style.transform = "rotate("+degrees+"deg)";
-
-	while (listener.id.offsetLeft < XXX && listener.id.offsetop > YYY){ //Last Move, back to center.
-		listener.id.offsetLeft -= 1;
-		listener.id.offsetTop += 1;
-	}
-
-	degrees += 90;
-	listener.id.style.transform = "rotate("+degrees+"deg)";
 }
--- js/SASscriptBACKUP.js
+++ js/SASscriptBACKUP.js
@@ -1,300 +1,124 @@
 //--- Load Audio files and position variables.
-var dragStatus;
-var degrees = 0;
-var demo = document.getElementById('demo');
+var Xdegree = 0;
+var Ydegree = 90;
 var context = window.AudioContext = window.AudioContext || window.webkitAudioContext;
+context = new AudioContext();
 var audioList = ['', 'audio/effect1.wav', 'audio/effect2.wav', 'audio/effect3.wav', 'audio/effect4.wav' ];
 var audiostatus = 'off';
-var listener = {x : '',	y : '', id : ''};
-listener.id = document.getElementById('head');
-context = new AudioContext();
+var listener = {x : '',	y : ''};
 var contextListener = context.listener;
-var audio = new Array(4);
+var audio = new Array();
 var source = new Array(4);
 var sound = new Array(4);
 var panner = new Array(4);
 for (var i=1; i<5; i++){
-	audio[i] = new Audio();
-	audio[i].src = audioList[i];
-	audio[i].loop = true;
+	var baseString = "#Src";
+	audio[baseString + i] = new Audio();
+	audio[baseString + i].src = audioList[i];
+	audio[baseString + i].loop = true;
 	panner[i] = context.createPanner();
-	panner[i].connect(context.destination);
-	source[i] = {x : '', y : '', src : '', distX : '', distY : '' };
+	panner[i].connect(context.destination);	
+	source[i] = {x : '', y : '', src : '' };
 	source[i].src = document.getElementById("Src" + i);
-	sound[i] = context.createMediaElementSource(audio[i]);
+	sound[i] = context.createMediaElementSource(audio[baseString + i]);
 	sound[i].connect(panner[i]);
+	panner[i].setPosition(source[i].src.offsetLeft, source[i].src.offsetTop, 1);
 }
 
 //Listener attributes.
-
+console.log(Xdegree,Ydegree,Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)))
 contextListener.setPosition(460,300,0);
-//contextListener.setOrientation(0,0,-1,0,1,0);
+contextListener.setOrientation(Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)),-1,0,1,0);
 
 //--- Drag elements with defined classes.
-function startDrag(e) {
+$("#head").draggable({
+	containment: "#grid",
+	scroll: false,	
+	start: function(event, ui) {
+		$(this).addClass('noclick');
+	},
+	drag: positionListener
+});
 
-	dragStatus = 1;
-	
-	//Determine event object.
-	if (!e) {
-		var e = window.event;
-	}
-	if(e.preventDefault) e.preventDefault();
+$(".src").draggable({
+	containment: "#grid",
+	scroll: false,	
+	start: function(event, ui) {
+		$(this).addClass('noclick');
+	},
+	drag: positionSources
+});
 
-	// IE uses srcElement, others use target.
-	targ = e.target ? e.target : e.srcElement;
-	if (targ.className != 'draggable' ) {
-		if (targ.className != 'src draggable' ) {return};
-	};
-
-	//Calculate event X, Y coordinates.
-	offsetX = e.clientX;
-	offsetY = e.clientY;
-
-	//Assign default values for top and left properties.
-	if(!targ.style.left) { targ.style.left='0px'};
-	if (!targ.style.top) { targ.style.top='0px'};
-
-	//Calculate integer values for top and left properties.
-	coordX = parseInt(targ.style.left);
-	coordY = parseInt(targ.style.top);
-	drag = true;
-
-	//Move element.
-	document.onmousemove = dragDiv;
-	return false;
+function positionListener(event, ui){
+	listener.x = ui.position.left;
+	listener.y = ui.position.top;
+	console.log(listener.x, listener.y);
+	contextListener.setPosition(listener.x, listener.y, 0);
 }
 
-function dragDiv(e) {
-	if (!drag) {return};
-	if (!e) { var e= window.event};
-	
+function positionSources(event, ui){
+	console.log(event.target.id, ui.position.left, ui.position.top)
+	var src = event.target.id;
+	var srcNumber = src.match(/\d/g);
 
-	positionSources()
-	positionListener()
-
-	//Move element
-	targ.style.left=coordX+e.clientX-offsetX+'px';
-	targ.style.top=coordY+e.clientY-offsetY+'px';
-
-	return false;
-}
-
-function stopDrag() {
-	drag=false;
-	dragStatus = 0;
-}
-window.onload = function() {
-	document.onmousedown = startDrag;
-	document.onmouseup = stopDrag;
-}
-
-function orientationListener(){ //The orientation will change as the Listener turns.
-	//contextListener.setOrientation(0,0,-1,0,1,0);
-}
-
-function positionListener(){
-	obj = listener.id;
-	listener.x = obj.offsetLeft;
-	listener.y = obj.offsetTop;
-	contextListener.setPosition(listener.x,listener.y,0);
-}
-
-function positionSources(){
-	for (var i=1; i<5; i++){
-		obj = source[i].src;
-		source[i].x = obj.offsetLeft;
-		source[i].y = obj.offsetTop;
-		panner[i].setPosition(source[i].x, source[i].y, 1);
-	}
-}
-
-//$( "head" ).draggable({ containment: "#grid-snap", scroll: false })
-
-/*function headingTo() {
-
-	for (var i=1; i<5; i++){
-		source[i].distX = Math.abs(listener.x - source[i].x);
-		source[i].distY = Math.abs(listener.y - source[i].y);
-	}
-
-	if (source[1].distX < source[2].distX){	
-		console.log('Listener heading to SRC1')		
-		factorX[1] += -0.012;
-		//if (source[1].distX < 70){
-		//	factorX[1] = 465;
-		//}
-
-	} else	{ //Make it clearer to hear SRC2.
-		console.log('Listener heading to SRC2')
-		factorX[1] += +0.012;
-		factorX[2] = 450
-		factorX[2] += +0.012;
-		if (source[1].distX < 70){
-			factorX[1] = 459;
-			factorX[2] = 455;
-		}
-
-
-	}
-	console.log(factorX[1], factorX[2])
-	panner[1].setPosition(factorX[1], 300, 0);
-	panner[2].setPosition(factorX[2], 300, 0);
-
-	if (source[1].distX < source[2].distX){ //Make it clearer to hear western objects.
-		c[2] = c[2] - 0.006; 
-		f[4] = f[4] + 0.006; 
-		console.log(c[2], f[4]);	
-		console.log('Listener heading west.') 
-		panner[2].setPosition(c[2], 1, 0);
-		panner[4].setPosition(f[4], 1, 0);
-		if (source[1].distY < source[3].distY){ //Make it clearer to hear SRC1.
-			c[1] = c[1] - 0.006; 
-			f[3] = c[1] + 0.006; 
-			//console.log(c[1], f[3]);
-			console.log('Listener heading to SRC1')
-			panner[1].setPosition(c[1], 0, 0);
-			panner[3].setPosition(f[3], 0.5, 0);
-		} else	{ //Make it clearer to hear SRC3.
-			c[3] = c[3] - 0.006; 
-			f[1] = c[3] + 0.006; 
-			console.log(c[3], f[1]);
-			console.log('Listener heading to SRC3')
-			panner[1].setPosition(f[1], 0.5, 0);
-			panner[3].setPosition(c[3], 0, 0);
-		}	
-	} else  { //Make it clearer to hear eastern objects.
-		c[1] = c[1] - 0.006; 
-		f[3] = f[3] + 0.006; 
-		console.log(c[1], f[3]);
-		console.log('Listener heading east.')
-		panner[1].setPosition(-0.2, 0, 0);
-		panner[3].setPosition(-0.1, 0, 0);
-		if (source[2].distY < source[4].distY){ //Make it clearer to hear SRC2.
-			c[2] = c[2] - 0.006; 
-			f[4] = f[4] + 0.006; 
-			console.log(c[2], f[4]);
-			console.log('Listener heading to SRC2')
-			panner[2].setPosition(0.1, 0, 0);
-			panner[4].setPosition(1, 0, 0);	
-		} else	{ //Make it clearer to hear SRC4.
-			c[2] = c[2] - 0.006; 
-			f[4] = f[4] + 0.006;  
-			console.log(c[2], f[4]);
-			console.log('Listener heading to SRC4')
-			panner[2].setPosition(0.1, 0, 0);
-			panner[4].setPosition(1, 0, 0);	
-		}
-	}
-
-	if (source[2].distX < 70 || source[4].distX < 70 || source[2].distY < 70 || source[4].distY < 70){
-		c[2] = f[2] = c[4] = f[4] = 1;
-	}
-
-	if (source[1].distX < 70 || source[3].distX < 70 || source[1].distY < 70 || source[3].distY < 70){			
-		c[1] = f[1] = c[3] = f[3] = -1;		
-	}
-}*/
-
-
-demo.onclick = function(){
-	console.log (' Demo Function')
-}
-
-listener.id.onclick = function(){
-	if (dragStatus == 0){
-		degrees += 90;
-		this.style.transform = "rotate("+degrees+"deg)";
-		contextListener.setOrientation(0,0,-1,0,1,0);
-	}
-}
-
-source[1].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[1].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[1].src.classList.add('srcplay');
-			//Play it.
-			audio[1].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[1].src.classList.add('srcplay');
-			audio[1].play();
-		}
-	} else if (source[1].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[1].pause();	
-		source[1].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
-}
-
-source[2].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[2].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[2].src.classList.add('srcplay');
-			//Play it.
-			audio[2].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[2].src.classList.add('srcplay');
-			audio[2].play();
-		}
-	} else if (source[2].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[2].pause();	
-		source[2].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
-}
-
-source[3].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[3].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[3].src.classList.add('srcplay');
-			//Play it.
-			audio[3].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[3].src.classList.add('srcplay');
-			audio[3].play();
-		}
-	} else if (source[3].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[3].pause();	
-		source[3].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
-}
-
-source[4].src.onclick = function(){
-	if (dragStatus == 1){
-		return false;
-	}
-	if (!source[4].src.classList.contains("srcplay")) {
-		if (audiostatus == 'off') {
-			source[4].src.classList.add('srcplay');
-			//Play it.
-			audio[4].play();
-			return false;
-		} else if (audiostatus == 'on') {
-			source[4].src.classList.add('srcplay');
-			audio[4].play();
-		}
-	} else if (source[4].src.classList.contains("srcplay")) {
-		//Stop it.
-		audio[4].pause();	
-		source[4].src.classList.remove('srcplay');
-		audiostatus = 'off';
-	}
+	srcNumber = parseInt(srcNumber.join(""));
+	source[srcNumber].x = ui.position.left;
+	source[srcNumber].y = ui.position.top;
+	panner[srcNumber].setPosition(source[srcNumber].x, source[srcNumber].y, 1);
 }
 
 //--- Turn listener.
+$('#head').click(function() {
+	if ($(this).hasClass('noclick')){
+		$(this).removeClass('noclick');
+	}else {
+		Xdegree += 90;
+		Ydegree += 90;		
+		console.log(Xdegree,Ydegree,Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)))
+		this.style.transform = "rotate("+-Xdegree+"deg)";
+		contextListener.setOrientation(Math.cos(Xdegree * (Math.PI / 180)),Math.cos(Ydegree * (Math.PI / 180)),-1,0,1,0);
+	}
+});
+
+//--- Demonstration function.
+$('#demo').click(function() {
+	console.log ('Demo Function')
+});
+
+//--- Play/Stop Sound sources on click.
+$('.src').click(function(event, ui) {
+	if ($(this).hasClass('noclick')) {
+		$(this).removeClass('noclick');
+	}else {
+		var Source = String('#'+ event.target.id);
+		var song;
+
+		song = audio[Source];
+		console.log();
+		playPause(event, song, audiostatus)
+	}
+});
+
+function playPause(event, song, audiostatus){
+	var Source = String('#'+ event.target.id);
+	console.log(Source)
+	//who is Source
+	if (!$(Source).hasClass("srcplay")) {
+		console.log(Source)
+		if (audiostatus == 'off') {
+			console.log(Source)
+			$(Source).addClass('srcplay');
+			//Play it.
+			song.play();
+			return false;
+		} else if (audiostatus == 'on') {
+			$(Source).addclass('srcplay');
+			song.play();
+		}
+	} else {
+		//Stop it.
+		song.pause();	
+		$("Source").removeClass('srcplay');
+		audiostatus = 'off';
+	}
+}
